3869c7a2c740bd24cf786d5bffcfd49ed24b3e1e
James Townsend <j.a.townsend2000@gmail.com>
2020-11-03 10:36:07 +0000

    PEP 8 compliance 2

--- part-ii/server.py
+++ part-ii/server.py
@@ -13,7 +13,7 @@ def log_message(message):
     print(output, end="")  # Log it to the console
 
 
-def find_nick(nick): # Quick and dirty linear search to get client from nickname
+def find_nick(nick):  # Quick and dirty linear search to get client from nickname
     for client, nickname in nicks.items():
         if nickname == nick:
             return client
@@ -59,31 +59,31 @@ def handle_connection(client):
             send_to_all(announcement + "\n")
             log_message(announcement)
             break
-        elif message_decode == "/err": # Should only occur if there is an issue with message receiving
+        elif message_decode == "/err":  # Should only occur if there is an issue with message receiving
             log_message("Communication error with client " + nicks[client] + ". A message was probably dropped :(")
             long_send(client, "[ERROR] Error receiving message from client.")
-        elif message_decode.startswith("/whis"): # Whisper function
-            target_client = find_nick(message_decode.split()[1]) # Finds the client with the target nickname
-            if not target_client: # If the target is None
-                long_send(client, "[ERROR] User not found!".encode()) # Inform the user as such
-                continue # Stop dealing with this message
-            message_body = message_decode[6:].partition(" ")[2] # Get body of the message
-            prefixed_message = "[" + nicks[client] + "] [WHISPER] " + message_body # Apply prefix and whisper tag
-            long_send(target_client, prefixed_message.encode()) # Send whisper to target
-            long_send(client, ("[SERVER] Whisper to " + nicks[target_client] + " sent.").encode()) # Send confirmation of whisper to sender
-            log_message(nicks[client] + " whispered to " + nicks[target_client]) # Log the occurrence of a whisper
-        elif message_decode.startswith("/here"): # Command to get connected clients
-            response_body = "\n\t".join(nicks.values()) # Get list of clients
-            long_send(client, ("[SERVER] Connected Clients:\n\t" + response_body).encode()) # Send that to the client
-            log_message(nicks[client] + " requested a list of clients.") # Log invocation of command
-        elif message_decode.startswith("/nick"): # Nickname command
-            new_nick = message_decode.split()[1] # Get nickname from message
-            if new_nick in nicks.values(): # If the nickname is taken
+        elif message_decode.startswith("/whis"):  # Whisper function
+            target_client = find_nick(message_decode.split()[1])  # Finds the client with the target nickname
+            if not target_client:  # If the target is None
+                long_send(client, "[ERROR] User not found!".encode())  # Inform the user as such
+                continue  # Stop dealing with this message
+            message_body = message_decode[6:].partition(" ")[2]  # Get body of the message
+            prefixed_message = "[" + nicks[client] + "] [WHISPER] " + message_body  # Apply prefix and whisper tag
+            long_send(target_client, prefixed_message.encode())  # Send whisper to target
+            long_send(client, ("[SERVER] Whisper to " + nicks[target_client] + " sent.").encode())  # Send confirmation of whisper to sender
+            log_message(nicks[client] + " whispered to " + nicks[target_client])  # Log the occurrence of a whisper
+        elif message_decode.startswith("/here"):  # Command to get connected clients
+            response_body = "\n\t".join(nicks.values())  # Get list of clients
+            long_send(client, ("[SERVER] Connected Clients:\n\t" + response_body).encode())  # Send that to the client
+            log_message(nicks[client] + " requested a list of clients.")  # Log invocation of command
+        elif message_decode.startswith("/nick"):  # Nickname command
+            new_nick = message_decode.split()[1]  # Get nickname from message
+            if new_nick in nicks.values():  # If the nickname is taken
                 long_send(client, "[ERROR] Nickname already taken.".encode())
             else:
-                log_message("[SERVER] " + nicks[client] + " has changed name to " + new_nick + ".") # Keep a log in the server
-                send_to_all("[SERVER] " + nicks[client] + " has changed name to " + new_nick + ".") # Alert everyone
-                nicks[client] = new_nick # Update the nickname on record
+                log_message("[SERVER] " + nicks[client] + " has changed name to " + new_nick + ".")  # Keep a log in the server
+                send_to_all("[SERVER] " + nicks[client] + " has changed name to " + new_nick + ".")  # Alert everyone
+                nicks[client] = new_nick  # Update the nickname on record
         else:
             prefixed_message = "[" + nicks[client] + "] " + message_decode
             send_to_all(prefixed_message)
